56b786a8ff04b25825d325f65cb8d10f08a29b72
Ivy Ti <chivy1221@gmail.com>
2021-01-15 09:52:35 +0200

    updated code

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,1 +1,35 @@
-const Posts = require('../models/posts');
+const Posts = require("../models/posts");
+
+const create = async (post) => {
+   return Posts.create({
+      title: post.title,
+      author: post.author,
+      timestamp: post.timestamp,
+      publishedDate: post.publishedDate,
+      isPublished: post.isPublished,
+   });
+};
+
+const find = async (info) => {
+   if (info.author && info.isPublished) {
+      if (typeof info.isPublished !== "boolean") {
+         return Posts.findAll({
+            attributes: ["author"],
+            order: ["id"],
+         });
+      } else {
+         return Posts.findAll({
+            attributes: ["isPublished", "author"],
+            order: ["id"],
+         });
+      }
+   } else
+      return Posts.findAll({
+         order: ["id"],
+      });
+};
+
+const post = async (id) => {
+   return Posts.findByPk(id);
+};
+module.exports = { create, find, post };
--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,55 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const postsController = require('../controllers/posts');
+const postsController = require("../controllers/posts");
 
+router.post("/", (req, res) => {
+   const post = {
+      title: req.body.title,
+      author: req.body.author,
+      timestamp: req.body.timestamp,
+      publishedDate: req.body.isPublished ? req.body.timestamp : null,
+      isPublished: req.body.isPublished,
+   };
+
+   postsController
+      .create(post)
+      .then((result) => {
+         res.status(201).json(result);
+      })
+      .catch((err) => {
+         res.status(err.status || 500).json(err);
+      });
+});
+
+router.get("/", (req, res) => {
+   //    console.log(req.query);
+   const info = {
+      author: req.query.author,
+      isPublished:
+         req.query.isPublished === "true"
+            ? true
+            : req.query.isPublished === "false"
+            ? false
+            : req.query.isPublished,
+   };
+   postsController.find(info).then((data) => {
+      res.status(200).json(data);
+   });
+});
+
+router.get("/:id", (req, res) => {
+   const id = req.params.id;
+   postsController.post(id).then((data) => {
+      if (data) res.status(200).json(data);
+      else res.status(404).json("ID not found");
+   });
+});
+
+router.put("/:id", (req, res) => {
+   res.status(405).json();
+});
+
+router.delete("/:id", (req, res) => {
+   res.status(405).json();
+});
 module.exports = router;
